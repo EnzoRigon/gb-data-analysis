fdb76b73b74f488f970d701b4453c9f6b5cf2868
Enzo Rigon <rossiporto1509@gmail.com>
2024-06-16 14:16:04 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+df = pd.read_csv('rendimentos-isentos-e-nao-tributaveis.csv', sep=';')
+
+
+def formatar_para_reais(valor):
+    return f'R$ {valor:,.2f}' if isinstance(valor, (int, float)) else valor
+
+
+st.title("Visualização de Dados")
+
+columns_to_display = [
+    'Lucros e dividendos recebidos', 
+    'Indenizações por rescisão de contrato de trabalho, inclusive a título de PDV, e por acidente de trabalho; e FGTS', 
+    'Ganho de capital na alienação de bem, direito ou conjunto de bens ou direitos da mesma natureza, alienados em um mesmo mês, de valor total de alienação até R$ 20.000,00, para ações alienadas no mercado de balcão, e R$ 35.000,00, nos demais casos', 
+    'Transferências patrimoniais - doações e heranças', 
+    'Rendimentos de cadernetas de poupança, letras hipotecárias, letras de crédito do agronegócio e imobiliário (LCA e LCI) e certificados de recebíveis do agronegócio e imobiliários (CRA e CRI)'
+]
+
+columns_to_display = [col for col in columns_to_display if col in df.columns]
+
+mandatory_columns = ['Ano Calendário', 'Faixa de Salários-Mínimos']
+
+st.write("Selecione a coluna que deseja visualizar:")
+selected_column = st.selectbox(
+    "Colunas disponíveis",
+    options=columns_to_display
+)
+
+if selected_column:
+    df['Ano Calendário'] = df['Ano Calendário'].astype(str)  
+
+    st.header("DataFrame e Média")    
+
+    col1, col2 = st.columns([2, 1])
+
+    with col1:
+        st.dataframe(df[mandatory_columns + [selected_column]].applymap(formatar_para_reais))
+
+    with col2:
+        average_value = df[selected_column].mean()
+        total_sum = df[selected_column].sum()
+
+        st.info(f"A média dos valores é: {formatar_para_reais(average_value)}")
+        st.success(f"Somatório Total: {formatar_para_reais(total_sum)}")
+
+    fig = px.line(df, x='Faixa de Salários-Mínimos', y=selected_column, title=f'Gráfico de {selected_column}',
+                    labels={selected_column: 'Reais', 'Faixa de Salários-Mínimos': 'Faixa de Salários-Mínimos'})
+
+    # Adicionar linha de média ao gráfico
+    fig.add_hline(y=average_value, line_dash="dot", line_color="red", annotation_text='Média',
+                    annotation_position="bottom right")
+
+    st.plotly_chart(fig)
+else:
+    st.write("Por favor, selecione pelo menos uma coluna para visualizar.")
